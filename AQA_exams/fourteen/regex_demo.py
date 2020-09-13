8efa011b5f34411f1ba4ecd62d79ae7eed6c34a5
kailash-manasarovar <niramisa777kubera@gmail.com>
2020-09-13 11:31:30 +0200

    Add regex demo

--- AQA_exams/fourteen/regex_demo.py
+++ AQA_exams/fourteen/regex_demo.py
@@ -1,1 +1,25 @@
-# todo:
+# https://www.w3schools.com/python/python_regex.asp
+# Chapter 20 page 165
+# https://docs.python.org/3/library/re.html
+import re
+
+# pattern = r"Cookie"
+# sequence = "Cookie"
+# if re.match(pattern, sequence):
+#     print("Match!")
+# else: print("Not a match!")
+
+
+txt = "The rain in Spain"
+x = re.search("^The.*Spain$", txt)
+if x:
+  print("YES! We have a match!")
+else:
+  print("No match")
+
+
+re.search(r'Co.k.e', 'Cookie').group()
+re.search(r'^Eat', "Eat cake!").group()
+re.search(r'cake$', "Cake! Let's eat cake").group()
+
+
