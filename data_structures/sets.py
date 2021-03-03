8aad4e15fc4c152285a9688a5132c0affb3bbacb
kailash-manasarovar <niramisa777kubera@gmail.com>
2021-03-03 13:01:22 +0100

    New gitignores

---
+++ data_structures/sets.py
@@ -0,0 +1,11 @@
+# unordered
+# unindexed
+# immutables (unchangeable)
+# no duplicates
+# any data type, mixed types too
+
+
+myset = {"apple", "banana", "cherry"}
+print(myset)
+print(type(myset))
+print(len(myset))
